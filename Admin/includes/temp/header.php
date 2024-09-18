be61e9fa98f013486dbddb82d5d48957e1953653
Mustafa Ashraf Nassar <106785472+MustfaAshraf@users.noreply.github.com>
2024-09-18 21:57:19 +0300

    Add files via upload

---
+++ Admin/includes/temp/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Native</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="includes/assets/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+</head>
+<body>
---
+++ Admin/includes/temp/navbar.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="dashboard.php">Dashboard</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="users.php">Users</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="categories.php">Categories</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="products.php">Products</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="messages.php">Messages</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="../logout.php">Logout</a>
+      </li>
+  </div>
+</nav>
